25ef7fcfcb233db9cb87cc97bf37701095b63d27
nochart <kannachart_apisit@kkumail.com>
2017-11-07 03:24:41 +0700

    พัง
    register พังหมดแล้ว

--- register.php
+++ register.php
@@ -21,15 +21,33 @@
 <h1>REGISTER</h1><br>
 <h4>PERSONAL DATA</h4><br>
 <div>
-<form>
-<input type="text" class="form-control" placeholder="Name" name="name" required /><br><br>
-<input type="text" class="form-control" placeholder="SSN" name="ssn" required /><br><br>
-<input type="text" class="form-control" placeholder="Address" name="address" required /><br><br>
-<input type="text" class="form-control" placeholder="Tel" name="tel" required /><br><br>
-<input type="text" class="form-control" placeholder="E-mail" name="email" required /><br><br>
-<input type="text" class="form-control" placeholder="Password" name="pass" required /><br><br>
-<input type="text" class="form-control" placeholder="Confirm Password" name="conpass" required /><br><br>
+<form >
+
+<div class="col-xs-4">
+<input type="text" class="form-control" placeholder="Name" name="name" required />
+</div><br><br>
+<div class="col-xs-4">
+<input type="text" id="i" class="form-control" 	placeholder="SSN" name="ssn" required />
+</div><br><br>
+<div class="col-xs-4">
+<input type="text" id="i" class="form-control" placeholder="Address" name="address" required />
+</div><br><br>
+<div class="col-xs-4">
+<input type="text" id="i" class="form-control" placeholder="Tel" name="tel" required />
+</div><br><br>
+<div class="col-xs-4">
+<input type="text" id="i" class="form-control" placeholder="E-mail" name="email" required />
+</div><br><br>
+<div class="col-xs-4">
+<input type="text" id="i" class="form-control" placeholder="Password" name="pass" required />
+</div><br><br>
+<div class="col-xs-4">
+<input type="text" id="i" class="form-control" placeholder="Confirm Password" name="conpass" required />
+</div><br><br>
 <a class="btn btn-warning" id="button" href="menu.php" role="button">Register</a>
+</div>
+</div>
+
 </form>
 </div>
 </body>
