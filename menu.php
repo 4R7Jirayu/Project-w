d7d947469c4dc0cddd6f8a4ce8b0fc8caa23683a
nochart <kannachart_apisit@kkumail.com>
2017-11-08 00:57:43 +0700

    เพิ่มเมนู
    เพิ่มหน้า Menu

--- menu.php
+++ menu.php
@@ -3,17 +3,96 @@
 <?php include "head.php";?>
 <style>
 #img{
-width: 150px;
+width: 80px;
 height: auto;
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
 }
+div{
+float:left;
+text-align:center;
+padding: 10px 40px;
+}
+
 </style>
 
-
-<div class="card" style="width: 20rem;">
+<div class="card" style="text-align: center;">
   <img id="img" class="card-img-top" src="images/car1.jpg" alt="Card image cap">
   <div class="card-body">
     <h4 class="card-title">Card title</h4>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+
+<div class="card" style="text-align: center;">
+  <img id="img" class="card-img-top" src="images/car1.jpg" alt="Card image cap">
+  <div class="card-body">
+    <h4 class="card-title">Card title</h4>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+
+<div class="card" style="text-align: center;">
+  <img id="img" class="card-img-top" src="images/car1.jpg" alt="Card image cap">
+  <div class="card-body">
+    <h4 class="card-title">Card title</h4>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+
+<div class="card" style="text-align: center;">
+  <img id="img" class="card-img-top" src="images/car1.jpg" alt="Card image cap">
+  <div class="card-body">
+    <h4 class="card-title">Card title</h4>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+
+<div class="card" style="text-align: center;">
+  <img id="img" class="card-img-top" src="images/car1.jpg" alt="Card image cap">
+  <div class="card-body">
+    <h4 class="card-title">Card title</h4>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+
+<div class="card" style="text-align: center;">
+  <img id="img" class="card-img-top" src="images/car1.jpg" alt="Card image cap">
+  <div class="card-body">
+    <h4 class="card-title">Card title</h4>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+
+<div class="card" style="text-align: center;">
+  <img id="img" class="card-img-top" src="images/car1.jpg" alt="Card image cap">
+  <div class="card-body">
+    <h4 class="card-title">Card title</h4>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+
+<div class="card" style="text-align: center;">
+  <img id="img" class="card-img-top" src="images/car1.jpg" alt="Card image cap">
+  <div class="card-body">
+    <h4 class="card-title">Card title</h4>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+
+<div class="card" style="text-align: center;">
+  <img id="img" class="card-img-top" src="images/car1.jpg" alt="Card image cap">
+  <div class="card-body">
+    <h4 class="card-title">Card title</h4>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+
+<div class="card" style="text-align: center;">
+  <img id="img" class="card-img-top" src="images/car1.jpg" alt="Card image cap">
+  <div class="card-body">
+    <h4 class="card-title">Card title</h4>
     <a href="#" class="btn btn-primary">Go somewhere</a>
   </div>
 </div>
