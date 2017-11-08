345ae7f8043ec9b27d51edc79312a013570a7dd3
nochart <kannachart_apisit@kkumail.com>
2017-11-09 03:05:30 +0700

    Form printBillAd

--- printBillAd.php
+++ printBillAd.php
@@ -1,19 +1,61 @@
+<style>
+h3{
+    display:block;
+}
+div{
+    margin-bottom:8px;
+    margin-top:8px;
+}
+span{
+    text-align: justify;
+}
+td{
+	text-align: center;
+}
+</style>
 <html> 
 <?php include "head.php"; ?>
+<?php 
+include "header.php";
+?>
 <title>Print Bill</title>
-<body> 
+<center>
+<div class="div">
+<h1 style="color:red">PRINT</h1>
+</div>
+<div>
+<h3 style="text-decoration: underline">Receipt</h3><br>
+<span style="display:inline-block">
+<input type=text name=foo id=foo placeholder=ID>
+</span><br>
+<span style="display:inline-block">
+<input type=text name=bar id=bar placeholder=DATE>
+</span><br>
+<span style="display:inline-block">
+<input type=text name=bar id=bar placeholder=TIME>
+</span><br>
+
 <div class="container">
-<div class="row">
-<div class="col-md-6 col-md-offset-3" >
-<div class="text-center">
-    <h1>Print Bill</h1>
-    <table class="table "> 
-    <tr class="text-center"> 
-        <td>#</td> <td>Name Menu</td> 
-        <td>Discription</td> 
-        <td>Price</td>         
-        <td>Action</td> 
-    </tr> 
+	<div class="row">
+	 <div class="col-md-10 col-md-offset-1">
+	 <div class="table-responsive">
+	 <table id="mytable" class="table table-striped" style="background-color:#33CC33">
+	  <tr>
+	  <th style="text-align:center">No.</th>
+	  <th style="text-align:center">Customer</th>
+	  <th style="text-align:center">CUST_ID</th>
+	  <th style="text-align:center">Price</th>
+	  </tr>
+	  <td>asdasd</td>
+	  <td>zxc</td>
+	  <td>asdl</td>
+	  <td>qwe</td>
+	  <tr>
+	<td colspan="3"><b>Total Price</b></td>
+    <td>---</td>
+  		</tr>
+
+
 <?php $sql = mysqli_query($conn,"Select * from student inner join major on student.major_id=major.major_id inner join faculty on faCulty.faculty_id=major.faculty_id ORDER BY std_id ASC"); 
     $n=1; 
     while($row = mysqli_fetch_array($sql))
@@ -47,6 +89,7 @@
 </div>
 </div>
 </div>       
-</div>		
-</body>
+</div>
+<a href="#" class="btn btn-primary btn-lg"><b>PRINT</b></a>	
+
 </html>
