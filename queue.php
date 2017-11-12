d3119daf9ebde3bd92e86af30ae723a1fd85ec9d
JP4R7 <artpp1996@gmail.com>
2017-11-12 20:38:19 +0700

    queue

--- queue.php
+++ queue.php
@@ -1,11 +1,13 @@
 <html>
-<?php include "head.php";?>
+<?php 
+    include "head.php";
+?>
 <?php 
 include "header.php";
 ?>
 
       <style>
-            td:hover{background-color:#ddfddd;;cursor: pointer}    
+             
             .not-active {
             pointer-events: none;
             cursor: default;
@@ -24,96 +26,92 @@ include "header.php";
       </tr>
     </thead>      
     <tbody id="bck">
-<script>
-      $(document).ready(function(){
-        $.ajax({                                      
-            url: 'apiQ.php',                  //the script to call to get data          
-            data: "",                        //you can insert url argumnets here to pass to api.php
-                                             //for example "id=5&parent=6"
-            dataType: 'json',                //data format  
-            defaultContent: '<button>Click!</button>',    
-            success: function(data)          //on recieve of reply
-            {   var tb = "";             
-                for(var i = 0; i < data.length; i++) {
-                  
-                 
-
-                    tb += "<tr>";
-                    tb += "<td width='40'>" + data[i].time + "</td>";
-                    if(data[i].bock1 == "ว่าง"){
-                        tb += "<td><a class='btn btn-md btn-block btn-success' href='confirmOrder.php?bock=bock1&time="+data[i].time+"'>" + data[i].bock1 + "</a></td>";
-                    }
-                    else{
-                        tb += "<td><a class='btn btn-md btn-block btn-danger disabled not-active' href='confirmOrder.php?bock=bock1&time="+data[i].time+"'>" + data[i].bock1 + "</a></td>";
-                   
-                    }
-                    if(data[i].bock2 == "ว่าง"){
-                        tb += "<td><a class='btn btn-md btn-block btn-success' href='confirmOrder.php?bock=bock2&time="+data[i].time+"'>" + data[i].bock2 + "</a></td>";
-                    }
-                    else{
-                        tb += "<td><a class='btn btn-md btn-block btn-danger disabled not-active' href='confirmOrder.php?bock=bock2&time="+data[i].time+"'>" + data[i].bock2 + "</a></td>";
-                   
-                    }
-                    if(data[i].bock3 == "ว่าง"){
-                        tb += "<td><a class='btn btn-md btn-block btn-success' href='confirmOrder.php?bock=bock3&time="+data[i].time+"'>" + data[i].bock3 + "</a></td>";
-                    }
-                    else{
-                        tb += "<td><a class='btn btn-md btn-block btn-danger disabled not-active' href='confirmOrder.php?bock=bock3&time="+data[i].time+"'>" + data[i].bock3 + "</a></td>";
-                   
-                    }                    
-                    tb += "</tr>";
-
-                }
-                
-               $("#bck").append(tb);
-                //$("#bck").append(tb);
-               // alert(tb);
-              //  $("#schedule").innerText  (tb);
-               // document.write(tb);
-            } 
-        });       
-    });  
-
-</script>
     </tbody>
       </table>
+      <button type="submit" class="btn btn-primary">ตกลง</button>
 </div>
 
 </body>
-
-
 <script>
-    var table = document.getElementById("schedule"),rIndex,cIndex;
+
+    var Ct =  "<?php echo $_GET['ct'] ?>";
+    var start;
+    var end;
+    var temp = 1;
+    var data;
+$(document).ready(function(){
     
-   
-            // table rows
-            for(var i = 1; i < table.rows.length; i++)
-            {
-                // row cells
-                for(var j = 0; j < table.rows[i].cells.length; j++)
-                {
-                    
-                    table.rows[i].cells[j].onclick = function()
-                    { 
-                        rIndex = this.parentElement.rowIndex;
-                        cIndex = this.cellIndex+1;
-                        console.log("Row : "+rIndex+" , Cell : "+cIndex);
-                       
-                        if($(this).text()=='ไม่ว่าง'){
-                          this.innerHTML="ว่าง";
-                          this.style.background="000000"  
-                        }
-                        else{
-                          this.innerHTML="ไม่ว่าง"
-                          this.style.background="red"   
-                        }
-                 };
-                }
+    $.ajax({                                      
+    url: 'apiQ.php',                  //the script to call to get data          
+                                       //you can insert url argumnets here to pass to api.php
+                                     //for example "id=5&parent=6"
+    dataType: 'json',                //data format       
+    success: function(data)          //on recieve of reply
+    {   var tb = "";             
+        for(var i = 0; i < data.length; i++) {         
+            tb += "<tr>";
+            tb += "<td width='50'>" + data[i].time + "</td>";
+            if(data[i].bock1 == "ว่าง"){
+                tb += "<td><a class='btn btn-md btn-block btn-success' onClick='return changecolor(this);' href='confirmOrder.php?time="+data[i].time+"'>" + data[i].bock1 + "</a></td>";
+                
             }
-                    
-                    
+            else{
+                tb += "<td><a class='btn btn-md btn-block btn-danger disabled not-active' href='confirmOrder.php?bock=bock1&time="+data[i].time+"'>" + data[i].bock1 + "</a></td>";
+           
+            }
+            if(data[i].bock2 == "ว่าง"){
+                tb += "<td><a class='btn btn-md btn-block btn-success' href='#' onClick='changecolor(this); '>" + data[i].bock2 + "</a></td>";
+               
+            }
+            else{
+                tb += "<td><a class='btn btn-md btn-block btn-danger disabled not-active' href='confirmOrder.php?bock=bock2&time="+data[i].time+"'>" + data[i].bock2 + "</a></td>";
+           
+            }
+            if(data[i].bock3 == "ว่าง"){
+                tb += "<td><a class='btn btn-md btn-block btn-success' href='#' onClick='return changecolor(this)'>" + data[i].bock3 + "</a></td>";
+               
+            }
+            else{
+                tb += "<td><a class='btn btn-md btn-block btn-danger disabled not-active' href='confirmOrder.php?bock=bock3&time="+data[i].time+"'>" + data[i].bock3 + "</a></td>";
+           
+            }                    
+            tb += "</tr>";
+
+        }        
+        $("#bck").append(tb);
+
+        } 
+    });       
+    
+});  
+
+
+
+function checkCt(num) {
+  if(num < 0){
+    alert("err");
+  }  
+   else{
+    return false;
+   }
    
+}
+
+function showtime(tm){
+    alert(tm);
+}
 
 
-        </script>
+function changecolor(element){    
+    if (element.innerHTML == "ว่าง" && Ct > 0){  
+        Ct--; 
+        element.innerHTML = "เลือก";
+        checkCt(Ct)
+      //  $("a").removeClass("btn btn-md btn-block btn-success").addClass("btn btn-md btn-block btn-primary");
+    } else {
+        element.innerHTML = "ว่าง"
+    }
+    return false;
+}
+</script>
 </html>
