a3f05c782af0ff7cc1f24efcb885ad78f3df1e08
JP4R7 <artpp1996@gmail.com>
2017-11-12 20:54:42 +0700

    revert Q

--- queue.php
+++ queue.php
@@ -1,4 +1,5 @@
 <html>
+<<<<<<< HEAD
 <?php 
     include "head.php";
 ?>
@@ -15,6 +16,15 @@ session_start();
             pointer-events: none;
             cursor: default;
             }       
+=======
+<?php include "../head.php";?>
+<?php 
+include "../header.php";
+?>
+
+      <style>
+            td:hover{background-color:#ddfddd;;cursor: pointer}           
+>>>>>>> parent of 86af630... queue
       </style>
 
 <body >
@@ -22,12 +32,13 @@ session_start();
   <table id="schedule" class="table" >
   <thead class="thead-dark">
       <tr>
-        <th scope="row">Time</th>
+        <th scope="col">Time</th>
         <th scope="col">Block 1 </th>
         <th scope="col">Block 2</th>
         <th scope="col">Block 3</th>
       </tr>
     </thead>      
+<<<<<<< HEAD
     <tbody id="bck">
     </tbody>
       </table>
@@ -103,6 +114,77 @@ function checkCt(num) {
 function showtime(tm){
     alert(tm);
 }
+=======
+    <tbody>
+    </tbody>
+    </table>
+</div>
+
+</body>
+
+
+<script>
+
+$(document).ready(function(){
+        $.ajax({                                      
+            url: 'apiQ.php',                  //the script to call to get data          
+            data: "",                        //you can insert url argumnets here to pass to api.php
+                                             //for example "id=5&parent=6"
+            dataType: 'json',                //data format      
+            success: function(data)          //on recieve of reply
+            {   var tb = "";
+
+                for(var i = 0; i < data.length; i++) {
+                    tb += "<tr>";
+                    tb += "<td>" + data[i].time + "</td>";
+                    tb += "<td>" + data[i].bock1 + "</td>";
+                    tb += "<td>" + data[i].bock2+ "</td>";
+                    tb += "<td>" + data[i].bock3+ "</td>";
+                    tb += "</tr>";
+                    
+                }
+                
+                $("#schedule").append(tb);
+              //  $("#schedule").innerText  (tb);
+               // document.write(tb);
+            } 
+        });       
+    });  
+
+
+    var table = document.getElementById("schedule"),rIndex,cIndex;
+
+          
+            // table rows
+            for(var i = 1; i < table.rows.length; i++)
+            {
+                // row cells
+                for(var j = 0; j < table.rows[i].cells.length; j++)
+                {
+                 
+                    table.rows[i].cells[j].onclick = function()
+                    { 
+                        rIndex = this.parentElement.rowIndex;
+                        cIndex = this.cellIndex+1;
+                        console.log("Row : "+rIndex+" , Cell : "+cIndex);
+                       
+                        if($(this).text()=='ไม่ว่าง'){
+                          this.innerHTML="ว่าง";
+                          this.style.background="000000"  
+                        }
+                        else{
+                          this.innerHTML="ไม่ว่าง"
+                          this.style.background="red"   
+                        }
+                        
+                        
+                 };
+
+                    
+                   
+                }
+            }
+>>>>>>> parent of 86af630... queue
 
 
 function changecolor(element){    
